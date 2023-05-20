2040c8f84b471f64947f5c9fc12405992318d501
Hao <objdev@outlook.com>
2023-05-20 17:29:04 +0800

    delete testing logs.

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,56 +1,24 @@
 (() => {
     if (window.hasRun) {
-        console.log("already run");
-
         return;
     }
 
     window.hasRun = true;
     window.controller = new AbortController();
-    console.log("enter the page");
 
     browser.runtime.onMessage.addListener((message) => {
         let body_el = document.getElementsByTagName("body")[0];
 
         // if px = -1, remove keydown listener.
         if (message.px === -1) {
-            console.log(message.px);
-            // document.removeEventListener("keydown", (e) => {
-            //     console.log("enter the key listener");
-
-            //     if (e.key === "ArrowDown") {
-            //         e.preventDefault();
-
-            //         console.log(message.px);
-            //         window.scrollBy(0, message.px);
-            //     }
-            // });
-
-            // document.getElementsByTagName("body")[0].removeEventListener("keydown", window.aa);
-            // document.getElementsByTagName("body")[0].replaceWith(document.getElementsByTagName("body")[0].cloneNode(true));
-
-            console.log("abort the key listener");
-
             window.controller.abort();
-
-            // if (keyListener) {
-            //     console.log("remove the key listener");
-
-            //     keyListener.remove();
-            // }
         } else {
-            console.log("signal is " + window.controller.signal.aborted);
-
             if (window.controller.signal.aborted) {
-                console.log("new controller");
-
                 window.controller = new AbortController();
             }
 
             // set the keydown listner.
             body_el.addEventListener("keydown", (e) => {
-                console.log("enter the key listener");
-
                 if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key)) {
                     let x, y = 0;
 
@@ -63,20 +31,9 @@
 
                     e.preventDefault();
 
-                    console.log(message.px);
                     window.scrollBy(x, y);
                 }
-
-                // if (e.key === "ArrowDown") {
-                //     e.preventDefault();
-
-                //     console.log(message.px);
-                //     window.scrollBy(0, message.px);
-                // }
             }, { signal: window.controller.signal });
         }
-
-        // console.log(message.px);
-        console.log(message.px === -1);
     });
 })();
--- popup/scroll_px.js
+++ popup/scroll_px.js
@@ -14,58 +14,33 @@ async function setPopupBackColor() {
 // initialize the popup window.
 // load data from the locally storage to the popup window.
 function initialize() {
-    console.log("initialize");
-
-    // todo: clean up all the locally storage data that is not today.
-    // browser.storage.local.clear();
+    // when popup window is loaded, get the focus on the input text box.
+    let px_box = document.getElementById("px_input");
+    px_box.focus();
+    px_box.select();
 
     browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-        console.log("tabs: " + tabs[0].id);
-        console.log("tabs type: " + typeof tabs[0].id);
-
         browser.storage.local.get(tabs[0].id.toString()).then((result) => {
             let keys = Object.keys(result);
 
             if (keys.length >= 1) {
                 let px_value = result[keys[0]];
-                console.log("px_value: " + px_value);
 
                 if (px_value) {
                     document.getElementById("px_input").value = px_value;
                     document.getElementById("arrow_chb").checked = true;
                 }
             }
-            console.log("result: " + result);
-            console.log("keys: " + Object.keys(result));
         });
     });
-
-    // browser.storage.local.get(null).then((result) => {
-    //     let keys = Object.keys(result);
-
-    //     console.log("key: " + keys);
-
-    //     for (let key of keys) {
-    //         console.log("key: " + key);
-    //         console.log("value: " + result[key]);
-    //     }
-    // });
 }
 
 // add click listener to the arrow button.
 function listenArrowBtn() {
-    // when popup window is loaded, get the focus on the input text box.
-    let px_box = document.getElementById("px_input");
-    px_box.focus();
-    px_box.select();
-
     let arrow_chb = document.getElementById("arrow_chb");
-    // let arrow_btn = document.getElementById("arrow_btn");
-    // let arrow_close_btn = document.getElementById("arrow_close_btn");
+    let px_box = document.getElementById("px_input");
 
     arrow_chb.addEventListener("change", () => {
-        console.log("arrow checkbox is " + arrow_chb.checked);
-
         if (arrow_chb.checked) {
             let px_value = px_box.value;
 
@@ -74,8 +49,6 @@ function listenArrowBtn() {
                 px_value = DEFAULT_PX;
                 px_box.value = px_value;
             }
-
-            document.getElementById("px_output").innerHTML = px_value;
 
             browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
                 let id = tabs[0].id;
@@ -89,33 +62,11 @@ function listenArrowBtn() {
             // -1 is the magic number. It means that the arrow button is disabled.
             browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
                 browser.storage.local.remove(tabs[0].id.toString()).then(() => {
-                    console.log("remove the px value from the local storage.");
-
                     browser.tabs.sendMessage(tabs[0].id, { px: -1 });
                 });
             });
         }
     });
-
-    // arrow_btn.addEventListener("click", function () {
-    //     let px_value = px_box.value;
-
-    //     if (isNaN(px_value)) {
-    //         px_value = DEFAULT_PX;
-    //     }
-
-    //     document.getElementById("px_output").innerHTML = px_value;
-
-    //     browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-    //         browser.tabs.sendMessage(tabs[0].id, { px: px_value });
-    //     });
-    // });
-
-    // arrow_close_btn.addEventListener("click", function () {
-    //     browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-    //         browser.tabs.sendMessage(tabs[0].id, { px: -1 });
-    //     });
-    // });
 }
 
 browser.tabs.executeScript({ file: "/content_scripts/content.js" }).then(listenArrowBtn);
